6f6b787923a18f602466955246aef99868052efe
JuanQuezada <ju.quezada.c@gmail.com>
2021-08-16 19:56:00 -0400

    first ccommit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //crear variables
-var jugadas = prompt('Indique cuantas veces desea jugar en esta partida');
+var jugadas = prompt('Indique cuantas veces desea jugar en esta partida', 3);
 var piedra = 0;
 var papel = 1;
 var tijera = 2;
@@ -8,6 +8,7 @@ var i = 1;
 // ciclo for para cantidad de juegos
 for (i = 1; i <= jugadas; i++)
 // do para entrar al ciclo
+
 do  
     { 
     var opcionUsuario = prompt("indique una opcion: \n¿piedra?: 0, \n¿papel?: 1  \n¿tijera?: 2", 1);    
@@ -15,48 +16,49 @@ do
     alert("Elegiste " + opciones[opcionUsuario]);
     alert("La Maquina eligio " + opciones[opcionMaquina]);
     i++ ;
-// if comparar opcion usuario con la opcion mathrandom    
+// if comparar opcion usuario con la opcion mathrandom 
+
 if(opcionUsuario == piedra)
 {
-    if(opcionMaquina == piedra)
+    if(opcionMaquina === piedra)
     {
         alert("EMPATE!");
     }
-    else if(opcionMaquina == papel)
+    else if(opcionMaquina === papel)
     {
         alert("HAS PERDIDO!!!");
     }
-    else if(opcionMaquina == tijera)
+    else if(opcionMaquina === tijera)
     {
         alert("HAS GANADO!!!");
     }
 }
 else if(opcionUsuario == papel)
 {
-    if(opcionMaquina == piedra)
+    if(opcionMaquina === piedra)
     {
         alert("HAS GANADO!!!");
     }
-    else if(opcionMaquina == papel)
+    else if(opcionMaquina === papel)
     {
         alert("EMPATE!");
     }
-    else if(opcionMaquina == tijera)
+    else if(opcionMaquina === tijera)
     {
         alert("HAS PERDIDO!!!!");
     }
 }
 else if(opcionUsuario == tijera)
 {
-    if(opcionMaquina == piedra)
+    if(opcionMaquina === piedra)
     {
         alert("HAS PERDIDO!!!");
     }
-    else if(opcionMaquina == papel)
+    else if(opcionMaquina === papel)
     {
         alert("HAS GANADO!!!");
     }
-    else if(opcionMaquina == tijera)
+    else if(opcionMaquina === tijera)
     {
         alert("EMPATE!");
     }
